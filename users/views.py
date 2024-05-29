ab44024bb18fd1516c57d54340e6fbb06000236e
noumanrehman042 <nouman.rehman@devsinc.com>
2024-05-29 19:30:22 +0500

    APP:Add login, register, and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .forms import UserRegisterForm
+
+
+class CustomLoginView(LoginView):
+    template_name = "users/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("posts:home")
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    template_name = "users/register.html"
+    success_url = reverse_lazy("posts:home")
+    form_class = UserRegisterForm
+    success_message = "Your profile was created successfully"
