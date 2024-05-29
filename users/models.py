ce336dcdbac1c8c195e463b24a17017c6d6cd01c
noumanrehman042 <nouman.rehman@devsinc.com>
2024-05-29 22:00:11 +0500

    PROFILE::Resize user profile pic after save

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,3 +10,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def save(self):
+        super().save()  # run save method the parent class
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            """ override the resized image and saved it in the file path"""
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
